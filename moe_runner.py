e2a6fdcb3bd1bc53ef0023589a40e8c302fbb407
Kent Slaney <kent@slaney.org>
2025-03-02 16:37:50 -0800

    initialize linear layers

--- moe_runner.py
+++ moe_runner.py
@@ -43,7 +43,7 @@ dataset.set_format(
 dataset = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=True)
 
-from deepseek import ModelArgs, Transformer
+from deepseek import ModelArgs, Transformer, Linear
 
 weight_decay = 1e-1
 beta1 = 0.9
@@ -58,12 +58,10 @@ class Trainer(Transformer):
         self.apply(self._init_weights)
 
     def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
+        if isinstance(module, Linear):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
-        elif isinstance(module, nn.Embedding):
-            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, tokens, targets=None, mask=None):
         logits = super().forward(tokens)
